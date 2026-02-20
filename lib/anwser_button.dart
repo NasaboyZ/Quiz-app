a1d5862464cc2421130122872e95a60a41a2cf66
NazaboyZ <josefleite.00@hotmail.com>
2026-02-20 20:01:05 +0100

    geting data from list

---
+++ lib/anwser_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnwserButton extends StatelessWidget {
+  const AnwserButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        backgroundColor: const Color.fromARGB(255, 71, 18, 131),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(33)),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quizapp/anwser_button.dart';
+import 'package:flutter_quizapp/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +12,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Quenstions screens');
+    final currentQuestions = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(currentQuestions.text, style: TextStyle(color: Colors.white)),
+          const SizedBox(height: 30),
+          AnwserButton(answerText: currentQuestions.answers[0], onTap: () {}),
+          const SizedBox(height: 10),
+          AnwserButton(answerText: currentQuestions.answers[1], onTap: () {}),
+          const SizedBox(height: 10),
+          AnwserButton(answerText: currentQuestions.answers[2], onTap: () {}),
+          const SizedBox(height: 10),
+          AnwserButton(answerText: currentQuestions.answers[3], onTap: () {}),
+        ],
+      ),
+    );
   }
 }
